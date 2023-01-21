ec466c3329ff69c24120d45cac71336131f99680
ElijahAllenBlackmon <elijahblackmon1010@gmail.com>
2023-01-21 15:01:31 -0600

    integrated read function for book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+book_path = 'books/frankenstein.txt'
+
+def main():
+    text = get_book_path(book_path)
+    print(text)
+
+
+def get_book_path(path):
+    with open(book_path) as f:
+        return f.read()
+
+main()
